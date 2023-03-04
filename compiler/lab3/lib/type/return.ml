62c00b926212fb2bb4037b9539b705f541b421bf
Theorem411 <ziqil2@andrew.cmu.edu>
2023-03-04 17:33:49 -0500

    fixed return06 bug

--- compiler/lab3/lib/type/return.ml
+++ compiler/lab3/lib/type/return.ml
@@ -1,15 +1,17 @@
 open Core
 module A = Aste
+
 let global_err = Error_msg.create ()
-;;
+
 exception ReturnError
-let error ~msg ~ast = 
+
+let error ~msg ~ast =
   Error_msg.error global_err (Mark.src_span ast) ~msg;
   raise ReturnError
 ;;
 
-let rec ret_checker_bool (prog : A.mstm) = 
-  match Mark.data prog with 
+let rec ret_checker_bool (prog : A.mstm) =
+  match Mark.data prog with
   | A.Declare decl -> ret_checker_bool decl.body
   | A.Assign _ -> false
   | A.If ifs -> ret_checker_bool ifs.lb && ret_checker_bool ifs.rb
@@ -20,14 +22,25 @@ let rec ret_checker_bool (prog : A.mstm) =
   | A.NakedExpr _ -> false
   | A.AssertFail -> true
   | A.NakedCall _ -> false
+;;
 
-let ret_checker_stm (prog : A.mstm) = 
-  if ret_checker_bool prog then ()
+let ret_checker_stm (prog : A.mstm) =
+  if ret_checker_bool prog
+  then ()
   else error ~msg:(sprintf "program does not have explicit return") ~ast:prog
 ;;
-let ret_checker_glob (mglob : A.mglob) =
-  match (Mark.data mglob) with
-  | A.Fundef { fdef; _ } -> ret_checker_stm fdef
-  | _ -> ()
 
-let ret_checker: A.program -> unit = List.iter ~f:ret_checker_glob;;
+let ret_checker_glob (mglob : A.mglob) =
+  let is_void_func (_, ret) =
+    match ret with
+    | None -> true
+    | Some _ -> false
+  in
+  match Mark.data mglob with
+  | A.Fundef { fdef; fsig; _ } ->
+    (* let () = printf "%s is (%b)-void type\n" (Ctype._fsig_tostring fsig) (is_void_func fsig) in  *)
+     if is_void_func fsig then () else ret_checker_stm fdef
+  | _ -> ()
+;;
+
+let ret_checker : A.program -> unit = List.iter ~f:ret_checker_glob
