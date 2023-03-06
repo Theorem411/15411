74cad75c57df22407dc93024a857ff86d42c135f
Ulan Seitkaliyev <ulanseitkaliyev@cmu.edu>
2023-03-05 22:55:15 -0500

    interface

--- compiler/lab3/lib/live/singlepass.mli
+++ compiler/lab3/lib/live/singlepass.mli
@@ -2,13 +2,10 @@ open Core
 module B = Block
 module V = Graph.Vertex
 
-include Identifiable.S with type t := Int.t
-
 type ht_entry = {
     test: int
 }
 
-val singlepass : _ Hashtbl.t -> B.fspace_block -> V.Set.t -> V.Set.t
+val singlepass : (int, ht_entry) Hashtbl.t -> B.block -> V.Set.t -> V.Set.t
 
-
-val get_edge_vertex: _ Hashtbl.t -> B.fspace_block -> (V.Set.t) * (V.t * V.t list)
+val get_edge_vertex: (int, ht_entry) Hashtbl.t -> B.block -> (V.Set.t) * (V.t * V.t list)
