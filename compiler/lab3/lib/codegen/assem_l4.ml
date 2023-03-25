4034abf1b9bc42583b0d918682d5b38a7aa6150e
Ulan Seitkaliyev <ulanseitkaliyev@cmu.edu>
2023-03-25 18:08:51 -0400

    adding formats

--- compiler/lab3/lib/codegen/assem_l4.ml
+++ compiler/lab3/lib/codegen/assem_l4.ml
@@ -24,6 +24,16 @@ type reg =
   | RBX
 [@@deriving equal, sexp, compare, enum, hash]
 
+let arg_i_to_reg = function
+  | 0 -> EDI
+  | 1 -> ESI
+  | 2 -> EDX
+  | 3 -> ECX
+  | 4 -> R8D
+  | 5 -> R9D
+  | _ -> failwith "args overflow 6"
+;;
+
 type ptraddr =
   | PtrTemp of
       { start : Temp.t
@@ -187,13 +197,144 @@ type fspace_block =
   ; fdef_block : block list
   }
 
-type fspace =
+(* type fspace =
   { fname : Symbol.t
   ; args : Temp.t list
   ; fdef : instr list
-  }
+  } *)
 
 type program_block = fspace_block list
+
+let format_reg r = sexp_of_reg r |> string_of_sexp
+let format_ptraddr = function
+  | PtrTemp { start; off } -> sprintf "%s + off(%d)" (Temp.name start) off
+  | PtrReg { start; off } -> sprintf "%s + off(%d)" (format_reg start) off
+;;
+
+let format_arraddr = function
+  | ArrTemp { start; off; typ; extra } ->
+    sprintf "start=%s; off=%d; typsz = %d; extra = %d" (Temp.name start) off typ extra
+  | ArrReg { start; off; typ; extra } ->
+    sprintf "start=%s; off=%d; typsz = %d; extra = %d" (format_reg start) off typ extra
+;;
+
+let format_addr = function
+  | Ptr p -> format_ptraddr p
+  | Arr a -> format_arraddr a
+;;
+
+let format_operand = function
+  | Imm n -> "$" ^ Int32.to_string n
+  | Temp t -> Temp.name t
+  | Reg r -> format_reg r
+  | Mem m -> "Mem(" ^ format_addr m ^ ")"
+;;
+
+let format_pure_operation = function
+  | Add -> "+"
+  | Sub -> "-"
+  | Mul -> "*"
+  | BitAnd -> "&"
+  | BitXor -> "^"
+  | BitOr -> "|"
+;;
+
+let format_efkt_operation = function
+  | Div -> "/"
+  | Mod -> "%"
+  | ShiftL -> "<<"
+  | ShiftR -> ">>"
+;;
+
+let format_unop = function
+  | BitNot -> "~"
+;;
+
+let format_instr' = function
+  | PureBinop binop ->
+    sprintf
+      "%s <-- %s %s %s"
+      (format_operand binop.dest)
+      (format_operand binop.lhs)
+      (format_pure_operation binop.op)
+      (format_operand binop.rhs)
+  | EfktBinop binop ->
+    sprintf
+      "%s <-- %s %s %s"
+      (format_operand binop.dest)
+      (format_operand binop.lhs)
+      (format_efkt_operation binop.op)
+      (format_operand binop.rhs)
+  | Unop unop ->
+    sprintf
+      "%s <-- %s%s"
+      (format_operand unop.dest)
+      (format_unop unop.op)
+      (format_operand unop.dest)
+  | Mov mv -> sprintf "%s <-- %s" (format_operand mv.dest) (format_operand mv.src)
+  | Directive dir -> sprintf "%s" dir
+  | Comment comment -> sprintf "/* %s */" comment
+  | Jmp l -> "jump" ^ Label.name l
+  | Cjmp c -> sprintf "%s %s" (c.typ |> sexp_of_jump_t |> string_of_sexp) (Label.name c.l)
+  | Lab l -> ".Label " ^ Label.name l
+  | Ret -> "ret"
+  | Set c ->
+    sprintf "%s %s" (c.typ |> sexp_of_set_t |> string_of_sexp) (format_operand c.src)
+  | Cmp (l, r) -> sprintf "cmp %s, %s" (format_operand l) (format_operand r)
+  | AssertFail -> "call __assert_fail"
+  | Call { fname; args_in_regs; args_overflow } ->
+    sprintf
+      "call %s(%s|%s)"
+      (Symbol.name fname)
+      (List.map args_in_regs ~f:format_reg |> String.concat ~sep:", ")
+      (List.map args_overflow ~f:Temp.name |> String.concat ~sep:", ")
+  | LoadFromStack ts ->
+    sprintf
+      "loadfromstack {%s}"
+      (List.map ts ~f:(fun t -> Temp.name t ^ ", ") |> String.concat)
+  | Alloc sz -> sprintf "alloc(%d)" sz
+  | Calloc { typ; len } -> sprintf "alloc_array[%s] of %d" (format_operand len) typ
+  | CheckNull p -> sprintf "check_if_null(%s)" (format_ptraddr p)
+  | CheckBound { base; idx } -> sprintf "check_bound(%s[%d])" (format_arraddr base) idx
+;;
+
+let format_instr i = format_instr' i ^ "\n"
+
+let format_jump_tag = function
+  | JRet -> "ret"
+  | JCon { jt; jf } -> sprintf "if(%s|%s)" (Label.name jt) (Label.name jf)
+  | JUncon l -> Label.name l
+;;
+
+let format_block ({ label; block; jump } : block) : string =
+  sprintf
+    "\n%s:\n%s\n%s\n"
+    (Label.name label)
+    (List.map block ~f:format_instr |> String.concat)
+    (format_jump_tag jump)
+;;
+
+let format_program_block prog_block =
+  let format_fspace_block { fname; args; fdef_block } =
+    sprintf
+      "%s(%s): \n%s"
+      (Symbol.name fname)
+      (List.map args ~f:Temp.name |> String.concat)
+      (List.map fdef_block ~f:format_block |> String.concat)
+  in
+  List.map prog_block ~f:format_fspace_block |> String.concat
+;;
+
+(* let format_program prog =
+  let format_fspace fspace =
+    sprintf
+      "%s(%s): \n%s"
+      (Symbol.name fspace.fname)
+      (List.map fspace.args ~f:Temp.name |> String.concat ~sep:",")
+      (List.map fspace.fdef ~f:format_instr |> String.concat)
+  in
+  List.map prog ~f:format_fspace |> String.concat
+;; *)
 
 (*_ comparable Make *)
 module T = struct
