bcb4086294f42f695b7212dccdd454febe051afa
Theorem411 <ziqil2@andrew.cmu.edu>
2023-04-02 19:19:04 -0400

    new stuff

--- compiler/lab3/lib/x86translation/regalloc.mli
+++ compiler/lab3/lib/x86translation/regalloc.mli
@@ -1,12 +1,12 @@
 module AS = Assem_l4
 module V = Graph.Vertex
 module TM = Temp.Map
-module Reg = Register
+module R = Register
 
 type color = (int[@deriving sexp, equal, compare])
 
 type reg_or_spill =
-  | Reg of Reg.reg_enum
+  | Reg of R.reg_enum
   | Spl of int
 
 val reg_alloc : AS.fspace -> reg_or_spill TM.t
