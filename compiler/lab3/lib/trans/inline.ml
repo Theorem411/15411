d9ec7b03c63e4a1d81b968dd90ae8eb307cdd024
Theorem411 <ziqil2@andrew.cmu.edu>
2023-04-11 17:08:40 -0400

    inline starterr

--- compiler/lab3/lib/trans/inline.ml
+++ compiler/lab3/lib/trans/inline.ml
@@ -1,5 +1,14 @@
 open Core
 module T = Tree_l4
 
-let inline : T.program -> T.program = failwith "no"
+module SM = Symbol.Map 
+
+let istarget (fspace : T.fspace_block) : Symbol.t * T.fspace_block = 
+  
+  failwith "no"
+
+
+let inline (prog : T.program) : T.program = 
+
+  failwith "no"
 ;;
