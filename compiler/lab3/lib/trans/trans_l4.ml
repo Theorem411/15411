f5b104ab07345129f738cca3e482cd1bdef1d56c
Ulan Seitkaliyev <ulanseitkaliyev@cmu.edu>
2023-04-10 23:48:52 -0400

    remove naked expr generating

--- compiler/lab3/lib/trans/trans_l4.ml
+++ compiler/lab3/lib/trans/trans_l4.ml
@@ -461,11 +461,13 @@ let rec tr_stm_rev
     let acc, b = tr_stm_rev env dep s1 acc_rev block_rev in
     let acc, b = tr_stm_rev env dep s2 acc b in
     acc, b
-  | NakedExpr exp ->
-    let t = Temp.create () in
+  | NakedExpr _ ->
+    (* let t = Temp.create () in
     let e, acc, b = tr_exp_rev env dep exp acc_rev block_rev in
     let b = { b with code = T.MovPureExp { dest = t; src = e } :: b.code } in
-    acc, b
+    acc, b *)
+    (* TRYING NOP NakedExpr  *)
+    acc_rev, block_rev
   | AssertFail ->
     let b = { block_rev with code = T.AssertFail :: block_rev.code } in
     acc_rev, b
