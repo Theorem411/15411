045810fadfcd4d53e4b2d3061d88f34effd446a3
Ulan Seitkaliyev <ulanseitkaliyev@cmu.edu>
2023-04-16 13:07:06 -0400

    delete old files

--- compiler/lab3/lib/trans/trans.mli
+++
@@ -1,5 +0,0 @@
-module A = Aste
-module T = Tree
-
-val translate : A.program -> T.program
-val translate_block : T.program -> T.program_block
--- compiler/lab3/lib/type/statsem.mli
+++
@@ -1,4 +0,0 @@
-module A = Aste
-
-(*_ raise exception when either type check, init check, or return check failed *)
-val static_semantic : hdr:A.program -> src:A.program -> unit
