786c21ec4846bca6bbd65540d3b452d446dbff11
Theorem411 <ziqil2@andrew.cmu.edu>
2023-04-16 16:14:37 -0400

    remove dangerous phiopt

--- compiler/lab3/lib/ssa/propagation.ml
+++ compiler/lab3/lib/ssa/propagation.ml
@@ -134,13 +134,13 @@ let target_second_phiopt ({ self; alt_selves } : SSA.phi) : (Temp.t * AS.operand
   let tset = TS.of_list tset in
   match List.length cset, AS.Set.length rset, TS.length tset with
   | 1, 0, 0 -> (* alt_selves only has one const *) Some (self, AS.Imm (List.hd_exn cset))
-  | 0, 0, 1 ->
+  (* | 0, 0, 1 ->
     (* alt_selves only has one temps excluding self *)
     if not (TS.mem tset self)
     then (
       let other = TS.to_list tset |> List.hd_exn in
       Some (self, AS.Temp other))
-    else None
+    else None *)
   | 0, 0, 2 ->
     (* alt_selves only has two temps including self *)
     if TS.mem tset self
