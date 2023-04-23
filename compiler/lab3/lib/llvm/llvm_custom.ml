3d8f993447a64d791d8f5518718346a5212ed931
Ulan Seitkaliyev <ulanseitkaliyev@cmu.edu>
2023-04-23 15:22:41 -0400

    push div

--- compiler/lab3/lib/llvm/llvm_custom.ml
+++ compiler/lab3/lib/llvm/llvm_custom.ml
@@ -467,9 +467,11 @@ let get_pre (file : string) : string =
        source_filename = \"%s\"\n\
        target datalayout = \
        \"e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128\"\n\
-       target triple = \"x86_64-pc-linux-gnu\"\n"
+       target triple = \"x86_64-pc-linux-gnu\"\n\
+      \ %s"
       file
       file
+      (format_pre ())
 ;;
 
 let get_post (_ : string) : string =
